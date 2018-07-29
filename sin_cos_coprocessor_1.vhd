f68a2e436b8e011d6615af0c162d5f8a5b0f92d7
Leonardo Iacovini <leoiacovini@gmail.com>
2018-07-29 16:32:31 -0300

    Add Behavioral cos

--- sin_cos_coprocessor_1.vhd
+++ sin_cos_coprocessor_1.vhd
@@ -44,10 +44,16 @@ port(	x: in std_logic_vector(15 downto 0);
 end sin_cos_coprocessor_1;
 
 architecture Behavioral of sin_cos_coprocessor_1 is
+    --sin
     constant CSIN1: real := 1.0;
     constant CSIN2: real := -1.0/6.0;
     constant CSIN3: real := 1.0/120.0;
     constant CSIN4: real := -1.0/5040.0;
+    -- cos
+    constant CCOS1: real := 1.0;
+    constant CCOS2: real := -1.0/2.0;
+    constant CCOS3: real := 1.0/24.0;
+    constant CCOS4: real := -1.0/720.0;
 
     signal real_x: real;
     signal real_x2: real;
@@ -75,13 +81,18 @@ begin
                     factor1 <= real_x3*CSIN2;
                     factor2 <= real_x5*CSIN3;
                     factor3 <= real_x7*CSIN4;
-
-                    result <= factor0 + factor1 + factor2 + factor3;
-                    r <= std_logic_vector(to_unsigned(integer(result*4096.0), 16));
                 else
                 --cos
+		    real_x4 <= real_x2*real_x2;
+                    real_x6 <= real_x4*real_x2;
+                    factor0 <= 1*CCOS1;
+		    factor1 <= real_x2*CCOS2;
+	            factor2 <= real_x4*CCOS3;
+                    factor3 <= real_x6*CCOS4;
                 end if;
-
+			
+                result <= factor0 + factor1 + factor2 + factor3;
+                r <= std_logic_vector(to_unsigned(integer(result*4096.0), 16));
             end if;
 
         end if;
